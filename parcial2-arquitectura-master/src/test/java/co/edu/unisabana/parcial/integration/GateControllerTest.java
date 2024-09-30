61e079d89f162a6ea02a5c29294f2f3a137e6527
Ana3Lu <anaquiva@unisabana.edu.co>
2024-09-30 09:02:07 -0500

    Integration Test

--- parcial2-arquitectura-master/src/test/java/co/edu/unisabana/parcial/integration/GateControllerTest.java
+++ parcial2-arquitectura-master/src/test/java/co/edu/unisabana/parcial/integration/GateControllerTest.java
@@ -1,10 +1,12 @@
 package co.edu.unisabana.parcial.integration;
 
 import co.edu.unisabana.parcial.controller.dto.CheckpointDTO;
+import co.edu.unisabana.parcial.controller.dto.ResponseGate;
 import co.edu.unisabana.parcial.repository.sql.jpa.CheckpointRepository;
 import co.edu.unisabana.parcial.controller.dto.CheckpointDTO;
 import co.edu.unisabana.parcial.repository.sql.jpa.CheckpointRepository;
 import co.edu.unisabana.parcial.repository.sql.entity.Checkpoint;
+import co.edu.unisabana.parcial.service.model.Checkin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,9 +38,9 @@ public class GateControllerTest {
 
     @Test
     void shouldCreateCheckpointSuccessfully() {
-        ResponseEntity<String> respuesta = testRestTemplate.postForEntity("/checkpoint", checkpointDTO, String.class);
+        ResponseEntity<ResponseGate> respuesta = testRestTemplate.postForEntity("/checkpoint/checkin", checkpointDTO, ResponseGate.class);
         Assertions.assertTrue(respuesta.getStatusCode().is2xxSuccessful());
-        Assertions.assertEquals("Checkpoint guardado exitosamente", respuesta.getBody());
+        Assertions.assertNotNull(respuesta.getBody());
     }
 
     @Test
@@ -49,6 +53,21 @@ public class GateControllerTest {
     void shouldNotCreateCheckpoint2() {
         checkpointDTO.dayOfMonth = 50;
         Assertions.assertThrows(IllegalArgumentException.class, () -> testRestTemplate.postForEntity("/checkpoint", checkpointDTO, String.class));
+        Assertions.assertTrue(checkpointRepository.findAll().isEmpty());
+    }
+
+    @Test
+    void shouldCreateCheckout() {
+        ResponseEntity<ResponseGate> respuesta = testRestTemplate.postForEntity("/checkpoint/checkout", checkpointDTO, ResponseGate.class);
+        Assertions.assertTrue(respuesta.getStatusCode().is2xxSuccessful());
+        Assertions.assertNotNull(respuesta.getBody());
+    }
+
+    @Test
+    void shouldNotCreateCheckout() {
+        checkpointDTO.dayOfMonth = 0;
+        Assertions.assertThrows(IllegalArgumentException.class, () -> testRestTemplate.postForEntity("/checkpoint/checkout", checkpointDTO, String.class));
+        Assertions.assertTrue(checkpointRepository.findAll().isEmpty());
     }
 }
 
